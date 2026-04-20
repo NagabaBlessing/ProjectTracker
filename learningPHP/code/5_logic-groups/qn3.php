e3d5d1682257c2b804b1f43f336b2fb37cf81776
NagabaBlessing <nagabablessing1@gmail.com>
2026-04-20 21:52:55 +0300

    Added gitignore,removed wordPress core files, allows only wpsetup and loop

--- learningPHP/code/5_logic-groups/qn3.php
+++
@@ -1,9 +0,0 @@
-<!-- Put your function from the previous exercise in one file. Then make another file
-that loads the first file and uses it to print out some <img /> tags. -->
-<?php
-include 'qn2.php';
-
-echo customImage("mountains.png", "Mountains", "500", "600");
-echo customImage("skydriving.png", "Vacation", "500", "600");
-
-?>
